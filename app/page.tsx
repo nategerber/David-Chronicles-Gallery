80d279739b0c120031c55a4b3ddb4775231ee5ae
nategerber <nategerber@gmail.com>
2024-09-30 02:45:09 -0400

    Create page.tsx
    The main page component.

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+// app/page.tsx
+import InteractiveFloorplan from '../components/interactive-floorplan-dark'
+
+export default function Home() {
+  return (
+    <main>
+      <InteractiveFloorplan />
+    </main>
+  )
+}
